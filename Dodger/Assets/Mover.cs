d2ef9b6264c910373a146388723e10a729a53dd6
AlessandroLuchian <alessandro_luchian@yahoo.com>
2023-12-28 18:44:54 +0100

    I added a better version of movement movePlayer and added a new skript that detects collision and added rigid body to an object called Pisti

--- Dodger/Assets/Mover.cs
+++ Dodger/Assets/Mover.cs
@@ -4,40 +4,20 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0.01f;
-    [SerializeField] float zValue = 0.01f;
+    [SerializeField] float moveSpeed = 10f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.W)){
-            transform.Translate(xValue,0,0);
-        }
-        if(Input.GetKey(KeyCode.S)){
-            transform.Translate(-xValue,0,0);
-        }
-        if(Input.GetKey(KeyCode.A)){
-            transform.Translate(0,0,zValue);
-        }
-        if(Input.GetKey(KeyCode.D)){
-            transform.Translate(0,0,-zValue);
-        }
-        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)){
-            transform.Translate(-xValue/2,0,zValue/2);
-        }
-        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)){
-            transform.Translate(-xValue/2,0,-zValue/2);
-        }
-        if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)){
-            transform.Translate(xValue/2,0,-zValue/2);
-        }
-        if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)){
-            transform.Translate(xValue/2,0,zValue/2);
-        }
+    void Update(){
+        movePlayer();
+    }
+
+    void movePlayer(){
+        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,0,zValue);
     }
 }
